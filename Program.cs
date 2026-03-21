9c6d3099a6dfc27126840d878bf3e21276dba01c
josemarbm <josemarbm23@gmail.com>
2026-03-21 19:21:28 -0300

    teste

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!") ERRO_DE_TESTE_AGENTE_AI
+        Console.WriteLine("Hello, World!"); 
         Console.ReadKey(); // Aguarda uma tecla antes de fechar
     }
 }
